e840b95aab566c9a564b1dcd742558ba85ddaa4a
Pavel235 <crushsad@gmail.com>
2023-12-30 13:15:34 +0200

    update js hw 10

---
+++ src/js/1-timer.js
@@ -0,0 +1,93 @@
+'use strict'
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const inputElement = document.querySelector('#datetime-picker');
+const StartBtn = document.querySelector('button[type="button"');
+const spanDays = document.querySelector('span[data-days]');
+const spanHours = document.querySelector('span[data-hours]');
+const spanMinutes = document.querySelector('span[data-minutes]');
+const spanSeconds = document.querySelector('span[data-seconds]');
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+     let userSelectedDate = selectedDates[0];
+     if(userSelectedDate <= new Date()) {
+       iziToast.error({
+        title: 'Error',
+        message: 'Please choose date in the future',
+       })
+       StartBtn.disabled = true; 
+     } else {
+        StartBtn.disabled = false; 
+     } 
+    },
+  };
+  
+  flatpickr(inputElement, options); 
+
+
+  StartBtn.addEventListener('click', () => {
+    startCountDown();
+    StartBtn.disabled = true; 
+  });
+
+  function startCountDown() {
+    const countDownInterval = setInterval(() => {
+        let SelectedDate = flatpickr.parseDate(inputElement.value);
+        const now = new Date();
+        const TimeDifference = SelectedDate - now; 
+
+        if(TimeDifference <= 0) {
+            clearInterval(countDownInterval);
+            updateTimer(0, 0, 0, 0);
+            iziToast.success({
+                title: 'Countdown finished',
+                message: 'The countdown has finished',
+            })
+        } else {
+            const {days, hours, minutes, seconds} = convertMs(TimeDifference);
+            updateTimer(days, hours, minutes, seconds); 
+        }
+    }, 1000)
+  }
+
+  function updateTimer (days, hours, minutes, seconds) {
+    spanDays.textContent = addLeadingZero(days);
+    spanHours.textContent = addLeadingZero(hours);
+    spanMinutes.textContent = addLeadingZero(minutes);
+    spanSeconds.textContent = addLeadingZero(seconds); 
+  }
+
+  function convertMs(ms) {
+
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds }; 
+  }
+
+  function addLeadingZero(value) {
+    return value.toString().padStart(2, '0'); 
+  }
+  
